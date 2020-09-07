b26f12f3bb0fca4433c356db8649b4a66b2d6b35
Jon Kunkee <jonathan.kunkee@gmail.com>
2020-09-07 11:45:52 -0700

    Improve logging function diversity
    Mostly separate Verbose and Ln differentiation

--- Log.h
+++ Log.h
@@ -9,26 +9,34 @@
 #if defined(DEBUG_LOGGING)
 
 #define Log(x) Serial.print(x)
+#define LogFile(x) { Log(__FILE__); Log(": "); Log(x); }
+#define LogFileFunc(x) { Log(__FILE__); Log(" "); Log(__func__); Log(": "); LogFile(x); }
 #define LogLn(x) Serial.println(x)
+#define LogLnFile(x) { LogFile(x); LogLn(""); }
+#define LogLnFileFunc(x) { LogFileFunc(x); LogLn(""); }
 
 #if defined(DEBUG_LOGGING_VERBOSE)
 
-#define LogLnVerbose(x) LogLn(x)
-#define LogLnFile(x) { Log(__FILE__); Log(": "); LogLn(x); }
-#define LogLnFileFunc(x) { Log(__FILE__); Log(" "); Log(__func__); Log(": "); LogLnFile(x); }
+#define LogVerbose(x) LogFileFunc(x)
+#define LogLnVerbose(x) LogLnFileFunc(x)
 
 #else // DEBUG_LOGGING_VERBOSE
 
-#define LogLnFile(x)
-#define LogLnFileFunc(x)
+#define LogVerbose(x)
+#define LogLnVerbose(x)
 
 #endif // DEBUG_LOGGING_VERBOSE
 
 #else // DEBUG_LOGGING
 
 #define Log(x)
+#define LogFile(x)
+#define LogFileFunc(x)
 #define LogLn(x)
 #define LogLnFile(x)
 #define LogLnFileFunc(x)
+
+#define LogVerbose(x)
+#define LogLnVerbose(x)
 
 #endif // DEBUG_LOGGING
