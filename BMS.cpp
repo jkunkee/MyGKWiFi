60ed77d3565b3747d23b0877e50305f04691cf09
Jon Kunkee <jonathan.kunkee@gmail.com>
2020-10-18 22:17:02 -0700

    Shave 300B .bss usage by dropping unused BMS reg definitions

--- BMS.cpp
+++ BMS.cpp
@@ -106,36 +106,36 @@ public:
 };
 
 static reg I2C_REG_VER(1, 0x00, true,  0x07, 0x07);
-static reg I2C_ADDRESS(1, 0x01, false, 0x43, 0x43);
+//static reg I2C_ADDRESS(1, 0x01, false, 0x43, 0x43);
 
-static reg LED_STATE  (1, 0x02, false, 0x01, 0x01);
+//static reg LED_STATE  (1, 0x02, false, 0x01, 0x01);
 
-static reg TOUCH_STATE(1, 0x3A, true,  0x00, 0x00);
-static reg TOUCH_CAP_CYCLES(1, 0x03, false, 0x00, 0x00);
-static reg TOUCH_THRESHOLD(1, 0x04, false, 12, 12);
-static reg TOUCH_HYSTERESIS(1, 0x05, false, 2, 2);
+//static reg TOUCH_STATE(1, 0x3A, true,  0x00, 0x00);
+//static reg TOUCH_CAP_CYCLES(1, 0x03, false, 0x00, 0x00);
+//static reg TOUCH_THRESHOLD(1, 0x04, false, 12, 12);
+//static reg TOUCH_HYSTERESIS(1, 0x05, false, 2, 2);
 
 // uC factory-calibrated values
 // static reg DCO_RSEL(1, 0x06, false, 0, 0); // factory-calibrated!
 // static reg DCO_DCOMOD(1, 0x07, false, 0, 0); // factory-calibrated!
 
-static reg VIN(2, 0x36, true, 0x00, 0x00);
-static reg VBAT(2, 0x32, true, 0x00, 0x00);
-static reg VOUT(2, 0x34, true, 0x00, 0x00);
-static reg IOUT(2, 0x38, true, 0x00, 0x00);
+//static reg VIN(2, 0x36, true, 0x00, 0x00);
+//static reg VBAT(2, 0x32, true, 0x00, 0x00);
+//static reg VOUT(2, 0x34, true, 0x00, 0x00);
+//static reg IOUT(2, 0x38, true, 0x00, 0x00);
 
-static reg VBAT_MIN(2, 0x08, false, 4665, 4665); // 4665==2.85V
-static reg VBAT_SHDN(2, 0x0a, false, 4829, 4829); // 4829==2.95V
-static reg VBAT_BOOT(2, 0x0c, false, 5156, 5156); // 5156==3.15V
-static reg VOUT_MAX(2, 0x0e, false, 5449, 5449); // 5449==3.5V
-static reg VIN_THRESHOLD(2, 0x10, false, 1451, 1451); // 1451==4.5V
-static reg IOUT_SHDN_THRESH(2, 0x1a, false, 0, 0); // 0==off
+//static reg VBAT_MIN(2, 0x08, false, 4665, 4665); // 4665==2.85V
+//static reg VBAT_SHDN(2, 0x0a, false, 4829, 4829); // 4829==2.95V
+//static reg VBAT_BOOT(2, 0x0c, false, 5156, 5156); // 5156==3.15V
+//static reg VOUT_MAX(2, 0x0e, false, 5449, 5449); // 5449==3.5V
+//static reg VIN_THRESHOLD(2, 0x10, false, 1451, 1451); // 1451==4.5V
+//static reg IOUT_SHDN_THRESH(2, 0x1a, false, 0, 0); // 0==off
 
-// battery factory-calibrated value
-//static reg VBAT_OFFSET(2, 0x012, false, 0, 0); // factory-calibrated!
-//static reg VOUT_OFFSET(2, 0x14, false, 0, 0); // factory-calibrated!
-//static reg VIN_OFFSET(2, 0x16, false, 0, 0); // factory-calibrated!
-static reg IOUT_OFFSET(2, 0x18, false, 0, 0); // actual default is 0
+//// battery factory-calibrated value
+////static reg VBAT_OFFSET(2, 0x012, false, 0, 0); // factory-calibrated!
+////static reg VOUT_OFFSET(2, 0x14, false, 0, 0); // factory-calibrated!
+////static reg VIN_OFFSET(2, 0x16, false, 0, 0); // factory-calibrated!
+//static reg IOUT_OFFSET(2, 0x18, false, 0, 0); // actual default is 0
 
 #define AUTO_BOOT_OFF 0x00
 #define AUTO_BOOT_VBAT 0x01
@@ -147,27 +147,26 @@ static reg IOUT_OFFSET(2, 0x18, false, 0, 0); // actual default is 0
 
 static reg AUTO_BOOT(1, 0x20, false, AUTO_BOOT_OFF, AUTO_BOOT_VIN);
 
-static reg WAKE_TIME(2, 0x26, false, 0, 0);
-static reg SHDN_DELAY(2, 0x1c, false, 65, 65); // ticks, 8 ticks/second
-static reg AUTO_SHDN_TIME(2, 0x1e, false, 0xFFFF, 0xFFFF); // 0xFFFF==off
+//static reg WAKE_TIME(2, 0x26, false, 0, 0);
+//static reg SHDN_DELAY(2, 0x1c, false, 65, 65); // ticks, 8 ticks/second
+//static reg AUTO_SHDN_TIME(2, 0x1e, false, 0xFFFF, 0xFFFF); // 0xFFFF==off
 
 static reg PI_BOOT_TO(1, 0x21, false, 30, 0); // 0==don't timeout if not acknowledged
 static reg PI_SHDN_TO(1, 0x22, false, 12, 0); // 0==don't timeout if not acknowledged
 
 static reg RTC_TIME(4, 0x28, false, 0, 0); // SHINY
-static reg RTC_WAKE_TIME(4, 0x2C, false, 0, 0);
+//static reg RTC_WAKE_TIME(4, 0x2C, false, 0, 0);
 
 #define WATCHDOG_OFF 0x00
 #define WATCHDOG_ALERT 0x01
 #define WATCHDOG_SHDN 0x02
-static reg WATCHDOG_CFG(1, 0x23, false, 0x00, 0x00);
-static reg WATCHDOG_GRACE(1, 0x24, false, 2, 2);
-static reg WATCHDOG_TIMER(1, 0x30, false, 2, 2); // initialized to WATCHDOG_GRACE on Pi boot
+//static reg WATCHDOG_CFG(1, 0x23, false, 0x00, 0x00);
+//static reg WATCHDOG_GRACE(1, 0x24, false, 2, 2);
+//static reg WATCHDOG_TIMER(1, 0x30, false, 2, 2); // initialized to WATCHDOG_GRACE on Pi boot
 
 static reg PI_RUNNING(1, 0x31, false, 1, 1); // gets set to 1 when Pi has booted by the daemon
-// SHINY
 
-static reg CFG_WRITE(1, 0x25, false, 0, 0); // write 0x46 to get commit flash-based settings to flash
+//static reg CFG_WRITE(1, 0x25, false, 0, 0); // write 0x46 to get commit flash-based settings to flash
 
 static boolean bms_reconfigure() {
   boolean retVal = true;
